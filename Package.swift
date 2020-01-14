77748980bac4134355484725f56c880f21c1a7a1
Yaroslav <yarodevuci@users.noreply.github.com>
2020-01-14 14:45:52 -0800

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "DropDown",
+    products: [
+        .library(name: "DropDown", targets: ["DropDown"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "DropDown", dependencies: [], path: "DropDown")
+    ]
+)
